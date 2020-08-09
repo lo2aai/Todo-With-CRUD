5f9cbe5a2514e41f8a195a05fa778b39840f0a89
Loai Amer <loai.amer@casita.com>
2020-08-10 00:53:32 +0200

    Todo App Is Ready For Push

---
+++ src/components/TodoItem.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '1rem',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none' 
+        }
+    }
+
+    render() {
+    const {id, title} = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+            <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
+            { title }
+            <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>Delete Item!</button>
+        </p>
+      </div>
+    )
+  }
+}
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    cursor: 'pointer',
+    float: 'right',
+    borderRadius: '3%',
+}
+
+// PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired,
+}
+
+export default TodoItem
